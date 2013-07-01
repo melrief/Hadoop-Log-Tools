aef63320a11c19406cf99823c558a9f2d736c292
Mario Pastorelli <Mario.Pastorelli@eurecom.fr>
2013-07-01 16:45:28 +0200

    first commit

---
+++ Setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from os import listdir
+from os.path import join
+
+setup(name='HadoopLogTools',
+      version='1.0',
+      description='Tools for Hadoop logs analisys',
+      author='Mario Pastorelli',
+      author_email='pastorelli.mario@gmail.command',
+      url='',
+      scripts=[join('bin',f) for f in listdir('bin')],
+      packages=['hadoop.log'
+               ,'hadoop.log.convert'
+               ,'hadoop.plot'],
+     )
