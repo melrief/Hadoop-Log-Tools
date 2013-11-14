220817ac8248f05e03ea5d74ecbf7bb2a25ae154
Mario Pastorelli <pastorelli.mario@gmail.com>
2013-11-14 19:23:42 +0100

    add job filtering in jobtimes

--- hadoop/log/jobtimes.py
+++ hadoop/log/jobtimes.py
@@ -3,7 +3,11 @@ from __future__ import print_function,division
 
 import argparse
 import json
+import os
 import sys
+
+from hadoop.util.filter import add_predicates
+
 
 def parse_args(args):
   p = argparse.ArgumentParser(description='Extract job times')
@@ -22,8 +26,13 @@ def parse_args(args):
   p.add_argument('-i','--input-files',metavar='INPUT_FILE'
                 ,required=True, nargs='+',type=argparse.FileType('rt')
                 ,help='files containing the job events in json format')
+  p.add_argument('-n','--print-id',action='store_true',default=False
+                ,help='print the jobid (or taskid) on each line before the number')
   p.add_argument('-t','--time-unit',default='seconds',choices=['seconds']
                 ,help='time unit to be displayed (for future releases)')
+
+  add_predicates(p)
+
   return p.parse_args(args)
 
 def getdiff(time_unit):
@@ -35,8 +44,17 @@ def getdiff(time_unit):
 def main():
   args = parse_args(sys.argv[1:])
   diff = getdiff(args.time_unit)
+  print_id = args.print_id
   for input_file in args.input_files:
-    job = json.load(input_file).values()[0]
+    jobid,job = json.load(input_file).items()[0]
+    
+    # filter by predicate
+    if args.predicate:
+      trueOrErr = args.predicate(job)
+      if trueOrErr != True:
+        sys.stderr.write('Ignoring job {} because {}{}'
+                            .format(jobid, trueOrErr, os.linesep))
+        continue
 
     # filter by num of maps
     if args.num_maps:
@@ -52,27 +70,41 @@ def main():
 
     # print the sojourn time
     if args.sojourn_time:
-      print(diff(job['finish_time'],job['submit_time']))
+      print('{} {}'.format(
+          jobid if print_id else ''
+        , diff(job['finish_time'],job['submit_time'])))
       continue
     
     # get a time task dependent
-    tasks = job['maps' if args.phase == 'map' else 'reduces'].values()
-    for task in tasks:
+    tasks = job['maps' if args.phase == 'map' else 'reduces'].items()
+    for taskid,task in tasks:
+      try:
 
-      # print the map or full_reduce time 
-      if args.phase == 'map' or args.phase == 'full_reduce':
-        print(diff(task['finish_time'],task['start_time']))
-      else:
-        attempt = task['successful_attempt'].values()[0]
-        # print the sort time
-        if args.phase == 'sort':
-          print(diff(attempt['sort_finished'],attempt['shuffle_finished']))
-        # print the shuffle time
-        elif args.phase == 'shuffle':
-          print(diff(attempt['shuffle_finished'],task['start_time']))
-        # print the reduce computation time
-        elif args.phase == 'reduce':
-          print(diff(task['finish_time'],attempt['shuffle_finished']))
+        # print the map or full_reduce time 
+        if args.phase == 'map' or args.phase == 'full_reduce':
+          print('{} {}'.format(
+              taskid if print_id else ''
+            , diff(task['finish_time'],task['start_time'])))
+        else:
+          attempt = task['successful_attempt'].values()[0]
+          # print the sort time
+          if args.phase == 'sort':
+            print('{} {}'.format(
+                taskid if print_id else ''
+              , diff(attempt['sort_finished'],attempt['shuffle_finished'])))
+          # print the shuffle time
+          elif args.phase == 'shuffle':
+            print('{} {}'.format(
+                taskid if print_id else ''
+              , diff(attempt['shuffle_finished'],task['start_time'])))
+          # print the reduce computation time
+          elif args.phase == 'reduce':
+            print('{} {}'.format(
+                taskid if print_id else ''
+              , diff(task['finish_time'],attempt['shuffle_finished'])))
+
+      except KeyError as ke:
+        sys.stderr.write('task: {} key_error:{}{}'.format(task,ke,os.linesep))
 
 if __name__=='__main__':
   main()
